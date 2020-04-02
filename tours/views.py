359d3b6904b43810c0d53a12e6f539e6ce897675
RubikOnLine <rrt@mail.ru>
2020-04-02 16:03:42 +0300

    1-й коммит. Подкорректированы urls.py, views.py и settings.py

---
+++ tours/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.views import View
+from .data import *
+
+# Create your views here.
+
+
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+
+        context ={}
+        context['tours'] = tours
+
+        return render(request, 'tours/index.html', context)
+
+class DepartureView(View):
+    def get(self, request, city_id:str):
+        context = {}
+        data = {}
+        i = 0
+
+        for key, value in tours.items():
+            if value["departure"] == city_id:
+                data[i] = value
+                i +=1
+                context['city'] = value['dep']
+
+        context['min_nights'] = min((data[i]["nights"]) for i in range(len(data)))
+        context['max_nights'] = max((data[i]["nights"]) for i in range(len(data)))
+        context['min_price'] = min((data[i]["price"]) for i in range(len(data)))
+        context['max_price'] = max((data[i]["price"]) for i in range(len(data)))
+        context['count'] = i
+        context['data'] = data
+
+
+        return render(request, 'tours/departure.html', context)
+
+class TourView(View):
+    def get(self, request, id):
+        context = {}
+
+        context['data'] = list(tours[id].values())
+
+        return render(request, 'tours/tour.html', context)
